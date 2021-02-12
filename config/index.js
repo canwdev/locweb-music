af6f0485c978fa7ceb868d2068ceeac8b32460aa
canwdev <c2097@live.com>
2021-02-12 10:27:56 +0800

    merge conflict

--- config/index.js
+++ config/index.js
@@ -2,9 +2,5 @@ const path = require('path')
 
 module.exports = {
   DATA_BASE_PATH: path.join(__dirname, '../data'),
-<<<<<<< HEAD
-  MUSIC_LIBRARY_PATH: '/run/media/can/OMEN_SEAGATE/Music/ACG/'
-=======
   MUSIC_LIBRARY_PATH: 'D:\\MusicCenter\\OST-Lite\\中国管弦乐纪念名盘.龙之吟'
->>>>>>> d1609b0b5fb40e0c809a4e57af5aa9808b517883
 }
