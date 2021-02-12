d1609b0b5fb40e0c809a4e57af5aa9808b517883
canwdev <c2097@live.com>
2021-02-12 10:19:59 +0800

    cross-env

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@ const path = require('path')
 
 module.exports = {
   DATA_BASE_PATH: path.join(__dirname, '../data'),
-  MUSIC_LIBRARY_PATH: '/run/media/can/OMEN_SEAGATE/Music/'
+  MUSIC_LIBRARY_PATH: 'D:\\MusicCenter\\OST-Lite\\中国管弦乐纪念名盘.龙之吟'
 }
