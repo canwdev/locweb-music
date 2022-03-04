3720c48a99ab1a1df82dd9d20677dd4593fd46db
canwdev <c2097@live.com>
2022-03-04 14:52:07 +0800

    eslint + prettier

---
+++ frontend-react/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  tabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  tailingComma: 'all',
+  bracketSpacing: false,
+  bracketSameLine: true,
+  arrowParens: 'always'
+};
