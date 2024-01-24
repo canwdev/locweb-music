3c036fff0f91e815b6819c1afc7fef9699c026ec
canwdev <c2097@live.com>
2024-01-24 21:06:56 +0800

    update: opus

--- frontend-vite-vue3/src/enum/file.ts
+++ frontend-vite-vue3/src/enum/file.ts
@@ -9,5 +9,5 @@ export interface BackendFileItem {
   mtime: string
 }
 
-export const supportedMusicFormat = /\.(mp3|wav|ogg|flac)$/i
+export const supportedMusicFormat = /\.(mp3|wav|ogg|flac|opus)$/i
 export const isSupportedMusicFormat = (name) => supportedMusicFormat.test(name)
